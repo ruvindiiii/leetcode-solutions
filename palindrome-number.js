853001265b866be70856275849d55de0a41b4f01
Ruvindi Hulangamuwa <ruvindihulangamuwa@gmail.com>
2024-05-22 22:35:05 +0200

    solutions for palindrome and two sum

---
+++ palindrome-number.js
@@ -0,0 +1,21 @@
+/**
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+ */
+
+// Answer
+
+var isPalindrome = function (x) {
+  const str = x.toString();
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+};
